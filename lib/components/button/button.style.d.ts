3fb75b1864f211ffae4b42106d6413090967b757
Sho Yamane <sns.yamane@gmail.com>
2021-03-17 11:01:57 +0900

    first commit

---
+++ lib/components/button/button.style.d.ts
@@ -0,0 +1,7 @@
+declare const _default: (data?: ({
+    color: string;
+} & {
+    theme?: any;
+}) | undefined) => import("jss").Classes<"label" | "root">;
+export default _default;
+//# sourceMappingURL=button.style.d.ts.map
